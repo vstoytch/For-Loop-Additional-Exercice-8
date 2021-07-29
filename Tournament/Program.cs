3fb0c77cd42f9f8cca73188be8f5a96e21307888
vstoytch <vstoytch@PMICHLAUL37172>
2021-07-29 16:39:40 +0200

    Add project files.

---
+++ Tournament/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Tournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            double numValuesToRead = double.Parse(Console.ReadLine());
+            double previousSum = 0;
+            double sum = 0;
+            
+            double maxDiff = 0;
+            bool sameSum = true;
+
+            for (int z = 1; z <= numValuesToRead; z++)
+            {
+
+                
+                    double N1 = double.Parse(Console.ReadLine());
+                    double N2 = double.Parse(Console.ReadLine());
+
+                    
+
+                     sum = N1 + N2;
+
+
+                    if (z == 1)
+                    {
+                        previousSum = sum;
+
+                       
+
+                    }
+
+
+
+
+                    if (sum != previousSum)
+                    {
+
+
+
+                    sameSum = false;
+                        double diff = Math.Abs(sum -previousSum);
+
+                        if (diff>maxDiff)
+                        {
+                            maxDiff = diff;
+                        }
+
+
+
+                    previousSum = sum;
+
+                    
+
+                }
+                    
+
+                }
+
+
+
+
+
+            
+
+
+
+
+
+            if (sameSum)
+            {
+                Console.WriteLine($"Yes, value={sum}");
+            }
+
+            else
+            {
+                Console.WriteLine($"No, maxdiff={maxDiff}");
+            }
+           
+
+
+
+
+
+
+
+
+        }
+    }
+}
